534dc2716ed185530c7c2c4c8cd990f5654db5b1
unknown <yeatz0408@gmail.com>
2022-02-02 07:42:46 +0900

    add VaribDeclnInit to javaBasics

---
+++ src/com/gmail/yeatz0408/javaBasics/VaribDeclnInit.java
@@ -0,0 +1,40 @@
+package com.gmail.yeatz0408.javaBasics;
+
+public class VaribDeclnInit {
+
+	public static void main(String[] args) {
+
+		int myNumber; //declaration
+		
+		myNumber = 10; // initialization
+		
+		boolean myBoolean1; boolean myBoolean2;
+		
+		// one line
+		double myDouble = 7.50;
+		
+		float myFloat1, myFloat2, myFloat3;
+		
+		double myDouble1, myDouble2 = 0.3;
+		
+		int myInt1 = 1, myInt2 = 2, myInt3 = 3;
+		
+		// int i3; i4;
+		
+		
+		// Only $ and _ can be used in variable names (it also can be used wherever)
+		
+		int ____anywhere____;
+		int $$$$$$$$;
+		int $_$_$_$_$_$_;
+		
+		// illegal examples
+		
+//		int 3point;
+//		int my@street;
+		
+		
+		
+	}
+
+}
