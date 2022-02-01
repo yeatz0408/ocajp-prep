7297446b1f5e1fc910b1980a7e629ef5ac75f424
unknown <yeatz0408@gmail.com>
2022-02-02 07:29:52 +0900

    add PrimitivesCharAndBoolean to javaBasics

---
+++ src/com/gmail/yeatz0408/javaBasics/PrimitivesCharAndBoolean.java
@@ -0,0 +1,24 @@
+package com.gmail.yeatz0408.javaBasics;
+
+public class PrimitivesCharAndBoolean {
+
+	public static void main(String[] args) {
+
+		char ch = 'a';
+		
+		char ch1 = '1';
+		char uniChar = '\u03A9'; //upper case greek omega character
+		char romanNumber = '\u216c'; //roman 50 number
+		
+		System.out.println(ch1);
+		System.out.println(uniChar);
+		System.out.println(romanNumber);
+		
+		boolean myBoolean = true;
+		boolean myFalseBoolean = false;
+		
+		// boolean true = false;
+		
+	}
+
+}
