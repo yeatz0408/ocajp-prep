0e5a27e8832d0098f511f78d9275032632cf196e
unknown <yeatz0408@gmail.com>
2022-02-03 07:41:16 +0900

    add arithmetic operators & Promotion to Operators class

--- src/com/gmail/yeatz0408/Operators.java
+++ src/com/gmail/yeatz0408/Operators.java
@@ -4,9 +4,48 @@ public class Operators {
 
 	public static void main(String[] args) {
 		
+		// Arithmetic operators
+		
+		int basics = 100 + 2 - 2 * 4 / 2;
+		int postIncrement = basics++;
+		int preIncrement = --basics;
+		int postDecrement = basics--;
+		int preDecrement = --basics;
+
+		int modules = 11 % 2;
+//		System.out.println(modules);
 		
 		
 		
+		// promotion
+  
+  		int intPromo = 5;
+  		double doublePromo = 5.55;
+  		
+ // 	System.out.println(intPromo + doublePromo);
+  		
+  		// int intDouble = 5 + doublePromo;
+  		int intDouble = 5 + (int) doublePromo;
+ // 	System.out.println(intDouble);
+  
+  		int bytePromo = 5;
+  		int byteInt = bytePromo + 5;
+ // 	System.out.println(byteInt);
+
+  		
+  		
+  		/*
+  		 Unary Operators
+  		 
+  		 
+  		 
+  		 
+  		 
+
+ 		*/
+
+
+
 
 	}
 
