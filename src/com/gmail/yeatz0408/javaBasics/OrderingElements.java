ce258bcbc68b05f0a37ba57197f6e1a8e60cf413
unknown <yeatz0408@gmail.com>
2022-02-02 09:31:39 +0900

    add OrderingElements to javaBasics

---
+++ src/com/gmail/yeatz0408/javaBasics/OrderingElements.java
@@ -0,0 +1,34 @@
+// import java.util.Date;*;
+
+package com.gmail.yeatz0408.javaBasics; // package  first (not mandatory)
+
+// int outsideClass = 3;
+
+import java.util.*; // import declaration second (not mandatory)
+
+class MultipleClasses{}
+
+// public MultiplePublicClass{}
+
+// class declaration third (madatory)
+public class OrderingElements {
+
+	// package com.gmail.yeatz0408;
+	
+	int myInt = 10;
+	
+	void method() {
+		
+	}
+
+	// method declaration can be done anywhere inside class declaration
+	public static void main(String[] args) {
+		
+		int methodInt;
+		
+//		void insideMethod() {
+//			
+//		}
+		
+	}
+}
