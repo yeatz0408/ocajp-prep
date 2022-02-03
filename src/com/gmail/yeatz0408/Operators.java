d4c066f17101c9db09eb0fbe6f80de92c3c2afb2
unknown <yeatz0408@gmail.com>
2022-02-03 10:52:24 +0900

    add assignment operators and compound assignment operators

--- src/com/gmail/yeatz0408/Operators.java
+++ src/com/gmail/yeatz0408/Operators.java
@@ -53,8 +53,58 @@ public class Operators {
   		// System.out.println(20 / --newInt);
   		
 
+  		
+  		
+  		
+  		/*
+  		 Assignment operators
+  		 */
+  		// overflow and underflow
+  		int x = (int) 1.0; // 1
+  		short y = (short) 324892482; // 20473
+  		int z = (int) 9.23f; // 9
+  		long t = 32849328329402L; 
+  		
+  		byte myByte = 127;
+  		byte myOtherByte = -128;
+  		
+  		// myByte = myByte + 1; // this doesn't work because 1 is an int as a literal
+  		// myOtherByte = myOtherByte - 1; // the same
+  		
+  		myByte++; // now -128
+  		myOtherByte--; // now +127
+  		
+  		// System.out.println(myByte);
+  		// System.out.println(myOtherByte);
+  		
+  		short short1 = 10;
+  		short short2 = 20;
+  		// short short3 = short1 * short2 // this doesn't work because short is promoted to int when used.
+  		short short3 = (short) (short1 * short2);
+  		
+  		
+  		
+  		/** 
+  		 * Compound Assignment Operators
+  		 */
 
-
+  		int compound = 2;
+  		  		
+  		compound += 3; // short for compound = compound + 3;
+  		compound -= 3; // short for compound = compound - 3;
+  		compound *= 3; // short for compound = compound * 3;
+  		compound /= 3; // short for compound = compound / 3;
+  		compound %= 3; // short for compound = compound % 3;
+  		
+  		long cLong = 10;
+  		int cInt = 4;
+  		
+  		// cInt = cInt * cLong; // doesn't work because cLong needs (int)
+  		cInt *=cLong;
+  		
+  		int mess = 4;
+  		int mess2 = (mess = 2); // this is legal.
+  		
 	}
 
 }
