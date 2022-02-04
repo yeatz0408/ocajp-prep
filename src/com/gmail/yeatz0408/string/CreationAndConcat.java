f37e2181a99c5778b04cd748f6b3ae646ae15855
unknown <yeatz0408@gmail.com>
2022-02-04 12:55:43 +0900

    add CreationAndConcat to string

---
+++ src/com/gmail/yeatz0408/string/CreationAndConcat.java
@@ -0,0 +1,26 @@
+package com.gmail.yeatz0408.string;
+
+public class CreationAndConcat {
+
+	public static void main(String[] args) {
+		
+		int number = 10;
+		int anotherNumber = 20;
+		System.out.println("result= " + (number + anotherNumber));
+		System.out.println("result= " + number + anotherNumber);
+		
+		
+		String str = "";
+		for (int i = 0; i < 10; i++) {
+			str += i + " ";
+		}
+		
+		System.out.println(str);
+		
+		String hello = "hello";
+		String world = "world";
+		String helloWorld = hello + world;
+
+	}
+
+}
