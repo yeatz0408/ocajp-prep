a6bb20d7e0e59f7024bd7964626fd7a54d321422
unknown <yeatz0408@gmail.com>
2022-02-04 11:00:14 +0900

    add DoWhileLoop to basicControlFlow

--- src/com/gmail/yeatz0408/basicControlFlow/DoWhileLoop.java
+++ src/com/gmail/yeatz0408/basicControlFlow/DoWhileLoop.java
@@ -4,7 +4,13 @@ public class DoWhileLoop {
 
 	public static void main(String[] args) {
 		
+		int x = 10;
 		
+		do {
+			System.out.println("x is already bigger than 5");
+			
+			x--;
+		} while (x < 5);
 
 	}
 
