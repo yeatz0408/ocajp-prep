1b107a71edad25904e508371b72d060cf486110e
unknown <yeatz0408@gmail.com>
2022-02-16 09:45:48 +0900

    add ReturnType to methods

---
+++ src/com/gmail/yeatz0408/methods/ReturnType.java
@@ -0,0 +1,29 @@
+package com.gmail.yeatz0408.methods;
+
+public class ReturnType {
+	
+	void jump() {}
+	
+	void jump2() {
+		return;
+	}
+	
+	String jump3() {
+		return "";
+	}
+	
+	int getInt() {
+		return 9;
+	}
+	
+	int getLong() {
+		return (int) 9L;
+	}
+	
+	boolean isTrue() {
+		return 5 == 5 ;
+	}
+	
+	void __test__() {}
+	void $$test$$() {}
+}
