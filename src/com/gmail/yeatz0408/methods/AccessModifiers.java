85066b8dac94bd9c11de5101c197441b09154a92
unknown <yeatz0408@gmail.com>
2022-02-16 10:12:34 +0900

    add AccessModifier to methods

---
+++ src/com/gmail/yeatz0408/methods/AccessModifiers.java
@@ -0,0 +1,19 @@
+package com.gmail.yeatz0408.methods;
+
+public class AccessModifiers {
+	
+	private int int1;
+	int int2;
+	protected int int3;	
+	public int int4;
+	
+	
+	private void jump() {}
+	
+	void jump1() {}
+	
+	protected void jump2() {}
+	
+	public void jump3() {}
+
+}
