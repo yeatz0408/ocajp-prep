355f8a34681e9e2a233ee96e88fb58dbf093d329
unknown <yeatz0408@gmail.com>
2022-02-16 09:30:07 +0900

    add MethodDesign to methods

---
+++ src/com/gmail/yeatz0408/methods/MethodDesign.java
@@ -0,0 +1,24 @@
+package com.gmail.yeatz0408.methods;
+
+public class MethodDesign {
+	
+	// mandatory: a return type, a method name, a pair of parameter brackets and a pair of body brackets
+	// optional: an access modifier, a final && / || static keyword, parameter type(s) + parameter name(s), a body
+	
+	public void jump() {}
+	
+	void jump1() {}
+	
+	// final can come anywhere except after return type
+	
+	public final void jump2() {}
+	
+	public static final void jump3() {}
+	
+	public final static void jump4() {}
+	
+	final public void jump5() {}
+	
+	
+
+}
