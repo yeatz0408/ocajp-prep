18aecf81547d6b177802a7261fe6038f2bbab69a
unknown <yeatz0408@gmail.com>
2022-02-22 09:51:10 +0900

    add CreatingDateAndTime to dateTime

---
+++ src/com/gmail/yeatz0408/dateTime/CreatingDateAndTime.java
@@ -0,0 +1,36 @@
+package com.gmail.yeatz0408.dateTime;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.Calendar;
+import java.util.Date;
+
+public class CreatingDateAndTime {
+
+	public static void main(String[] args) {
+		
+		System.out.println(LocalDate.now());
+		System.out.println(LocalTime.now());
+		System.out.println(LocalDateTime.now());
+		
+		LocalDate localDate = LocalDate.of(2017, Month.JANUARY, 1);
+		LocalTime localTime = LocalTime.of(10, 30, 23, 234234342);
+		
+		System.out.println(localDate);
+		System.out.println(localTime);
+		System.out.println(LocalDateTime.of(localDate, localTime));
+		
+		// before java 8
+		System.out.println(new Date());
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(2015, Calendar.JANUARY, 1);
+		
+		Date january = calendar.getTime();
+		System.out.println(january);
+
+	}
+
+}
